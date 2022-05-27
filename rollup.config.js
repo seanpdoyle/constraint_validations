6ece928b8bf61c6ce535032f76b351ecd960734a
Sean Doyle <sean.p.doyle24@gmail.com>
2022-05-27 15:22:13 -0400

    Add support for `import` through Import Maps
    Extend the Rollup configuration to also build an ES modules-compliant
    file with the `.es.js` suffix.
    
    Then, when consumer applications manage their JavaScript dependencies
    through [import maps][], they can pin the dependency to
    `constraint_validations.es.js`:
    
    ```ruby
    pin "constraint_validations", to: "constraint_validations.es.js"
    ```
    
    [import maps]: https://github.com/rails/importmap-rails#importmap-for-rails

--- rollup.config.js
+++ rollup.config.js
@@ -3,25 +3,41 @@ import sourcemaps from "rollup-plugin-sourcemaps"
 import { terser } from "rollup-plugin-terser"
 import pkg from "./package.json"
 
-export default {
-  input: pkg.module,
-  output: {
-    name: "ConstraintValidations",
-    file: pkg.main,
-    format: "iife",
-    inlineDynamicImports: true,
-    sourcemap: true
+export default [
+  {
+    input: pkg.module,
+    output: {
+      name: "ConstraintValidations",
+      file: pkg.main,
+      format: "iife",
+      inlineDynamicImports: true,
+      sourcemap: true
+    },
+    plugins: [
+      sourcemaps(),
+      resolve(),
+      terser({
+        mangle: false,
+        compress: false,
+        format: {
+          beautify: true,
+          indent_level: 2
+        }
+      })
+    ]
   },
-  plugins: [
-    sourcemaps(),
-    resolve(),
-    terser({
-      mangle: false,
-      compress: false,
-      format: {
-        beautify: true,
-        indent_level: 2
-      }
-    })
-  ]
-}
+  {
+    input: pkg.module,
+    output: {
+      name: "ConstraintValidations",
+      file: "app/assets/javascripts/constraint_validations.es.js",
+      format: "es",
+      inlineDynamicImports: true,
+      sourcemap: true
+    },
+    plugins: [
+      sourcemaps(),
+      resolve()
+    ]
+  }
+]
