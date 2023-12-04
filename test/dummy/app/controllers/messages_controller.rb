c6be497f159085e36ed1b629ccbae2970f3ca2bc
Sean Doyle <sean.p.doyle24@gmail.com>
2023-12-04 13:25:38 -0500

    Expand CI Matrix to Test against Turbo and Stimulus
    Add the `hotwire-enabled` setting to the CI matrix, then map that to the
    `HOTWIRE_ENABLED` environment variable.
    
    When `HOTWIRE_ENABLED="true"`, render the test page HTML with Turbo and
    Stimulus integration. Otherwise, render with built-in JavaScript.
    
    In the test harness, incorporate the `hotwire_enabled` query parameter
    override as a `HOTWIRE_ENABLED` corollary. Similarly, incorporate the
    `disableSubmitWhenInvalid` query parameter into the
    `ConstraintValidations` instance configuration.

--- test/dummy/app/controllers/messages_controller.rb
+++ test/dummy/app/controllers/messages_controller.rb
@@ -4,12 +4,22 @@ class MessagesController < ApplicationController
   end
 
   def create
-    message = Message.new(params.require(:message).permit(:subject, :contents))
+    message = Message.new(message_params)
 
     if message.valid?
-      redirect_back or_to: root_url
+      redirect_to new_message_url(redirect_params)
     else
       render :new, locals: {message: message}, status: :unprocessable_entity
     end
+  end
+
+  private
+
+  def message_params
+    params.require(:message).permit(:status, :subject, :content)
+  end
+
+  def redirect_params
+    params.to_unsafe_h.except(:action, :authenticity_token, :controller, :message)
   end
 end
