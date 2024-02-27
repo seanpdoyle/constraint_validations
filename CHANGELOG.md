91ffe1adb383d18ae5e505e41c1c13cc9e847730
Sean Doyle <sean.p.doyle24@gmail.com>
2024-02-27 11:28:37 -0500

    Skip `[disabled]` fields while validating
    First, define `focusFirstInvalidField` recursively so that
    `HTMLFormElement` arguments and `HTMLElement` arguments share the same
    code paths.
    
    Next, change `isFieldElement` to check for properties instance of class
    inheritance, including a check that `.disabled` is false and a check
    that `.willValidate` is false (to exclude `<fieldset>` elements).

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -6,6 +6,10 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ## [Unreleased]
 
+*   Ignore `[disabled]` fields while validating
+
+    *Sean Doyle*
+
 *   Resolve issue where `:index` was ignored for multiply-nested forms
 
     *Sean Doyle*
--- app/assets/javascripts/constraint_validations.js
+++ app/assets/javascripts/constraint_validations.js
@@ -117,21 +117,15 @@ var ConstraintValidations = function() {
     }
   }
   function focusFirstInvalidField(element) {
-    let firstInvalidField;
     if (element instanceof HTMLFormElement) {
-      for (const field of element.elements) {
-        if (field.validity.valid) {
-          continue;
-        } else {
-          firstInvalidField = field;
-          break;
-        }
-      }
-    } else if ("validity" in element && !element.validity.valid) {
-      firstInvalidField = element;
+      return Array.from(element.elements).some((field => focusFirstInvalidField(field)));
+    } else if (isFieldElement(element) && !element.validity.valid) {
+      element.focus();
+      element.scrollIntoView();
+      return true;
+    } else {
+      return false;
     }
-    firstInvalidField?.focus();
-    firstInvalidField?.scrollIntoView();
   }
   function disableSubmitWhenInvalid(form) {
     if (!form || form.noValidate) return;
@@ -161,7 +155,7 @@ var ConstraintValidations = function() {
     }
   }
   function isFieldElement(element) {
-    return [ HTMLButtonElement, HTMLInputElement, HTMLObjectElement, HTMLOutputElement, HTMLSelectElement, HTMLTextAreaElement ].some((field => element instanceof field));
+    return !element.disabled && "validity" in element && element.willValidate;
   }
   return ConstraintValidations;
 }();
